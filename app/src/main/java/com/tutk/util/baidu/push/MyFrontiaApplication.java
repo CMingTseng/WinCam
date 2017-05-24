42eb021307b2f865ad8c11e3f7a54b75a365b5d3
Neo <mingtseng.chang@huafu.com.tw>
2017-05-24 19:56:03 +0800

    re-location class

---
+++ app/src/main/java/com/tutk/util/baidu/push/MyFrontiaApplication.java
@@ -0,0 +1,13 @@
+package com.tutk.util.baidu.push;
+
+import com.baidu.frontia.FrontiaApplication;
+
+public class MyFrontiaApplication extends FrontiaApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
